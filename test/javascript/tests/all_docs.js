560cd6f7dc2ecfbc4129bb97cb23f6f5ce52cbe2
sebastianro <sebastianro@apache.org>
2015-04-08 09:53:08 +0200

    make all_docs.js cluster-capabable

--- test/javascript/tests/all_docs.js
+++ test/javascript/tests/all_docs.js
@@ -16,12 +16,23 @@ couchTests.all_docs = function(debug) {
   db.createDb();
   if (debug) debugger;
 
+  function wait(ms) {
+    var t0 = new Date(), t1;
+    do {
+      CouchDB.request("GET", "/");
+      t1 = new Date();
+    } while ((t1 - t0) <= ms);
+  }
+
   // Create some more documents.
   // Notice the use of the ok member on the return result.
   T(db.save({_id:"0",a:1,b:1}).ok);
   T(db.save({_id:"3",a:4,b:16}).ok);
   T(db.save({_id:"1",a:2,b:4}).ok);
   T(db.save({_id:"2",a:3,b:9}).ok);
+
+  // wait for the cluster to re-balance
+  wait(250);
 
   // Check the all docs
   var results = db.allDocs();
@@ -48,49 +59,94 @@ couchTests.all_docs = function(debug) {
   TEquals(0, raw.rows.length);
 
   // check that the docs show up in the seq view in the order they were created
+  // cluster can't guarantee a deterministic sequence! - we rather have to check whether every item is there exactly once
   var changes = db.changes();
-  var ids = ["0","3","1","2"];
+  var ids = [];
   for (var i=0; i < changes.results.length; i++) {
     var row = changes.results[i];
-    T(row.id == ids[i], "seq order");
+    ids.push(row.id);
   };
+  ids.sort();
+  T(JSON.stringify(ids) == JSON.stringify(["0", "1", "2", "3"]), "seq once");
 
   // it should work in reverse as well
+  // deterministic sequence applies here as well
   changes = db.changes({descending:true});
-  ids = ["2","1","3","0"];
+  ids = [];
   for (var i=0; i < changes.results.length; i++) {
     var row = changes.results[i];
-    T(row.id == ids[i], "descending=true");
+    ids.push(row.id);
   };
+  ids.sort();
+  T(JSON.stringify(ids) == JSON.stringify(["0", "1", "2", "3"]), "seq once");
 
   // check that deletions also show up right
+  // deterministic sequence - you know ;-)
   var doc1 = db.open("1");
   var deleted = db.deleteDoc(doc1);
   T(deleted.ok);
+  // wait for the cluster to re-balance
+  wait(500);
   changes = db.changes();
-  // the deletion should make doc id 1 have the last seq num
+  // the deletion should make doc id 1 have deletion
   T(changes.results.length == 4);
-  T(changes.results[3].id == "1");
-  T(changes.results[3].deleted);
+  var chgdoc1=null;
+  for(var i=0; i < changes.results.length; i++) {
+    if(changes.results[i].id == "1") {
+      chgdoc1=changes.results[i];
+      break;
+    }
+  }
+  T(chgdoc1!=null);
+  T(chgdoc1.id == "1");
+  T(chgdoc1.deleted);
 
   // do an update
   var doc2 = db.open("3");
   doc2.updated = "totally";
   db.save(doc2);
+  // wait for the cluster to re-balance
+  wait(500);
   changes = db.changes();
 
   // the update should make doc id 3 have the last seq num
   T(changes.results.length == 4);
-  T(changes.results[3].id == "3");
+  var chgdoc3=null;
+  for(var i=0; i < changes.results.length; i++) {
+    if(changes.results[i].id == "3") {
+      chgdoc3=changes.results[i];
+      break;
+    }
+  }
+  T(chgdoc3!=null);
+  T(chgdoc3.id == "3");
 
   // ok now lets see what happens with include docs
   changes = db.changes({include_docs: true});
   T(changes.results.length == 4);
-  T(changes.results[3].id == "3");
-  T(changes.results[3].doc.updated == "totally");
+  chgdoc3=null;
+  for(var i=0; i < changes.results.length; i++) {
+    if(changes.results[i].id == "3") {
+      chgdoc3=changes.results[i];
+      break;
+    }
+  }
+  T(chgdoc3!=null);
+  T(chgdoc3.id == "3");
+  T(chgdoc3.doc);
+  T(chgdoc3.doc.updated == "totally", "update not incl");
 
-  T(changes.results[2].doc);
-  T(changes.results[2].doc._deleted);
+  chgdoc1=null;
+  for(var i=0; i < changes.results.length; i++) {
+    if(changes.results[i].id == "1") {
+      chgdoc1=changes.results[i];
+      break;
+    }
+  }
+  T(chgdoc1!=null);
+  T(chgdoc1.id == "1");
+  T(chgdoc1.doc);
+  T(chgdoc1.doc._deleted);
 
   rows = db.allDocs({include_docs: true}, ["1"]).rows;
   TEquals(1, rows.length);
@@ -108,17 +164,27 @@ couchTests.all_docs = function(debug) {
   };
   T(db.save(conflictDoc1, {new_edits: false}));
   T(db.save(conflictDoc2, {new_edits: false}));
+  // wait for the cluster - you know ;-)
+  wait(500);
 
   var winRev = db.open("3");
 
   changes = db.changes({include_docs: true, conflicts: true, style: "all_docs"});
-  TEquals("3", changes.results[3].id);
-  TEquals(3, changes.results[3].changes.length);
-  TEquals(winRev._rev, changes.results[3].changes[0].rev);
-  TEquals("3", changes.results[3].doc._id);
-  TEquals(winRev._rev, changes.results[3].doc._rev);
-  TEquals(true, changes.results[3].doc._conflicts instanceof Array);
-  TEquals(2, changes.results[3].doc._conflicts.length);
+  chgdoc3=null;
+  for(var i=0; i < changes.results.length; i++) {
+    if(changes.results[i].id == "3") {
+      chgdoc3=changes.results[i];
+      break;
+    }
+  }
+  T(chgdoc3!=null);
+  T(chgdoc3.id == "3");
+  TEquals(3, chgdoc3.changes.length);
+  TEquals(winRev._rev, chgdoc3.changes[0].rev);
+  TEquals("3", chgdoc3.doc._id);
+  TEquals(winRev._rev, chgdoc3.doc._rev);
+  TEquals(true, chgdoc3.doc._conflicts instanceof Array);
+  TEquals(2, chgdoc3.doc._conflicts.length);
 
   rows = db.allDocs({include_docs: true, conflicts: true}).rows;
   TEquals(3, rows.length);
